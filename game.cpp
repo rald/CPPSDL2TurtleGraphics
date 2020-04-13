5bbe95737da0041622798de119f26f0e2285d215
root <root@localhost>
2020-04-14 07:46:46 +0800

    update

--- game.cpp
+++ game.cpp
@@ -1,26 +1,29 @@
 #include "Turtle.hpp"
 
-const int SCREEN_WIDTH=1024;
-const int SCREEN_HEIGHT=768;
+int main(void)
+{
 
-int main(void) {
+	const int SCREEN_WIDTH = 1024;
+	const int SCREEN_HEIGHT = 768;
 
-	Turtle::world(SCREEN_WIDTH,SCREEN_HEIGHT);
+	Turtle::world(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-	Turtle *turtle=new Turtle(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,0);
+	Turtle *turtle = new Turtle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 0);
 
 	Turtle::start();
 
-	int size=1;
+	int size = 1;
 
 	turtle->setPenColor(0xFFFFFFFF);
-	for(int i=0;i<100;i++) {
+	for (int i = 0; i < 100; i++)
+	{
 		turtle->move(size);
 		turtle->turn(91);
-		size+=5;
+		size += 5;
 	}
 
 	Turtle::end();
-	
+
 	return 0;
 }
+
